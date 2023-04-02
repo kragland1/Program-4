f1ae8aa70f7c0b211afc5d62cc10e1fb132894bf
kelec <kelec@LAPTOP-JGFOV90A>
2023-04-02 17:03:47 -0400

    Add project files.

---
+++ Program 4/Program.cs
@@ -0,0 +1,57 @@
+﻿// Grading ID: R2226
+// Program 4
+// Due: 4/18/22
+// CIS 199 - 02
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Program_4
+{
+    internal class Program
+    {
+        // PreCondition: None
+        // PostCondition: Creating many superhero objects using the Superhero class structure.
+        static void Main(string[] args)
+        {
+            //creating the superhero objects.
+            Superhero superhero1 = new Superhero("Batman", "Gotham City", "Expert Martial Artist", "Genius", 1939); //  Batman Information
+            Superhero superhero2 = new Superhero("Spider-Man", "Queens, New York City", "Super Strength", "Wall Crawling Ability", 1962); // Spider-Man information
+            Superhero superhero3 = new Superhero("Daredevil", "Hells Kitchen, New York City", "Enhanced Senses", "Great Martial Artist", 1964); // Daredevil information
+            Superhero superhero4 = new Superhero("Superman", "Krypton", "Super Strength", "Super Speed", 1938); // Superman Information
+            Superhero superhero5 = new Superhero("The Flash", "Central City", "Super Speed", "Super Speed Thinking", 1940); // The Flash Information
+
+            superhero4.FirstSuperPower = "Heat Vision"; // This is set here to hard code the first superpower for the Superman object/
+            superhero5.OnPlanetEarth(); // Used to change the information for "The Flash" information to say that he is on earth.
+            superhero3.OnPlanetEarth(); // Used to change the information for the "Daredevil" information to say that he is on earth.
+
+            // created a 5 element Superhero array.
+            Superhero[] superheroes = new Superhero[5];
+            
+            //initializing the array with the types that was decalred for the objects.
+            superheroes[0] = superhero1;
+            superheroes[1] = superhero2;
+            superheroes[2] = superhero3;
+            superheroes[3] = superhero4;
+            superheroes[4] = superhero5;
+
+            PrintSuperHero(superheroes); // calls the array and outputs it the way that the PrintSuperHero method organized the data.
+
+        }
+        //PreCondition: None
+        //PostCondition: Used to print the information from the array using a static void print method and a foreach loop.
+        public static void PrintSuperHero(Superhero[] superheroes)
+        {
+            // Processes each element in the array with a loop
+            foreach (Superhero hero in superheroes)
+            {
+                WriteLine($"{hero}"); // calls on the ToString from the Superhero class to the main one.
+            }
+        }
+        
+    }
+}
---
+++ Program 4/Superhero.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4
+{
+    class Superhero
+    {
+        private int year;// From the year 0 to beyond.
+
+        bool _PlanetEarth;// A bool operator for whether a hero is on the planet earth or not.
+
+        // PreCondition: None
+        // PostCondition: The _supername string has been returned.
+        public string SuperheroName { get; set; } // PreCondition: None; PostCondition: The _supername string is set to the value specified
+
+        // PreCondition: None
+        // PostCondition: The _birthCity string has been returned.
+        public string BirthCity { get; set; } // PreCondition: None; PostCondition: The _birthCity string is set to the value specified
+
+        // PreCondition: None
+        // PostCondition: The second string has been returned.
+        public string SecondSuperPower { get; set; } // PreCondition: None; PostCondition: The second string is set to the value specified
+
+        // PreCondition: None
+        // PostCondition: The first string has been returned.
+        public string FirstSuperPower { get; set; } // PreCondition: None; PostCondition: The first string is set to the value specified
+
+        // PreCondtion: None
+        // PostCondition: The Superhero is made using objects that provide the specified name, birth city, first superpower, second superpower and the year that superhero was created.
+        public Superhero(string _supername, string _birthCity, string first, string second, int year)
+        {
+            SuperheroName = _supername; // Set using the SuperheroName property
+            BirthCity = _birthCity; // Set using the BirthCity property
+            FirstSuperPower = first; // Set using the FirstSuperPower property
+            SecondSuperPower = second; // Set using the SecondSuperPower property
+            BirthYear = year; // Set using the BirthYear property
+        }
+
+        // PreCondition: None
+        // PostCondition: The Initial Property uses the SuperheroName property value and chooses the first initial of the string name.
+        public char Initial
+        {
+            get { return SuperheroName[0]; } // This will return the Superhero Name's first initial.
+        }
+
+        public const int Default = 1999; // This sets a default integer for 1999
+
+        public int BirthYear
+        {
+            // PreCondition: None
+            // PostCondition: Returns the year
+            get { return year; }
+
+            // PreCondition: 0 <= value 
+            // PostCondition: The year has been set to the specified value.
+            set
+            {
+                if (value >= 0)
+                {
+                    year = value;
+                }
+                else //When the value is not greater than 0 or it is not a number then set the Default to 1999.
+                {
+                    year = Default;
+                }
+            }
+        }
+
+        //PreCondition: None
+        //PostCondition: Makes the _PlanetEarth bool variable true if the superhero is declared to be On Earth.
+        public void OnPlanetEarth()
+        {
+            _PlanetEarth = true;
+        }
+
+        // PreCondition: None
+        // PostCondition: Makes the _PlanetEarth bool variable false if the superhero is declared to not be on Earth.
+        public void OffPlanetEarth()
+        {
+            _PlanetEarth = false;
+        }
+
+        //PreCondition: None
+        //PostCondition: Returns the bool _PlanetEarth
+        public bool IsOnPlanetEarth()
+        {
+            return _PlanetEarth;
+        }
+
+        //PreCondition: None
+        //PostCondition: Returns the superhero information as a formatted string.
+        public override string ToString()
+        {
+            string NL = Environment.NewLine;// This is used to create a line for each string.
+            return $"Name: {SuperheroName} {NL}" + $"City: {BirthCity} {NL}" + $"First Super Power: {FirstSuperPower} {NL}" + $"Born in: {BirthYear} {NL}" + $"Second Super Power: {SecondSuperPower} {NL}" + $"Planet Earth: {_PlanetEarth} {NL}" + $"Initial: {Initial} {NL}";
+        }
+    }
+}
